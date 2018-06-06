0959e7e48f5afb4ff61c67b8acc5118012e3b714
Lukas F. Hartmann <lukas@mnt.mn>
2018-06-06 21:07:22 +0200

    updated BOM

--- spreadsheets/fetch.sh
+++ spreadsheets/fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O production_status.csv "https://docs.google.com/spreadsheets/d/1o5MmcjnUXUqvDNSovIvVbP7XQIZMDLPQgBvyF0soFjs/export?format=csv"
-wget -O motherboard_bom.csv "https://docs.google.com/spreadsheets/d/13ZvuRw21PMBu9xKJPVixFvNTR7WqUfe5qeK15DIdoU4/export?format=csv"
+wget -O motherboard_bom.csv "https://docs.google.com/spreadsheets/d/17emwhTD4pCTshw2-VjbxPvs5wxsENxl2uvpOU9lgOwY/export?format=csv"
