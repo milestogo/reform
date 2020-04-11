2f83c572742a77c97cebc6053724b44143195eb0
mntmn <lukas@mntmn.com>
2020-04-11 20:43:46 +0200

    reform2-motherboards: cleanup and rename

--- spreadsheets/fetch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -O production_status.csv "https://docs.google.com/spreadsheets/d/1o5MmcjnUXUqvDNSovIvVbP7XQIZMDLPQgBvyF0soFjs/export?format=csv"
-wget -O motherboard_bom.csv "https://docs.google.com/spreadsheets/d/17emwhTD4pCTshw2-VjbxPvs5wxsENxl2uvpOU9lgOwY/export?format=csv"
